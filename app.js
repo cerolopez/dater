db6786c40f2cf83af0dae0b282e04ed4276d77d2
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-10-20 21:07:23 -0700

    Connecting MongoDB

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 app.use(express.static('public'));
+const { MongoClient } = require("mongodb");
+const uri = "mongodb+srv://fulltimers4real:gejg9wDDBJNgvMw@3rdwheel.ohwtksk.mongodb.net/?retryWrites=true&w=majority";
+const databaseName = "3rdwheel";
+
 app.listen(3000, () => {
-   console.log('Server listening on 3000');
+    console.log('Server listening on 3000');
+ });
+
+MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
+  if (error) {
+    return console.log("Connection failed for some reason");
+  }
+  console.log("Connection established - All well");
+  const db = client.db(databaseName);
 });
 
+app.
+
 /*
-app.get('/', function(req, res) {
-    res.sendFile('index.html', { root: '.' });
-    //res.sendFile('style.css', { root: '.' });
-  });
-  */
-
-
   app.get('/love', (req, res) => {
     res.send('Hi Love');
   });
+  */
