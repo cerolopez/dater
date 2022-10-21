2ca857df5dd3c5af8b6c34d09287bf80f18b8ed0
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-10-20 19:22:52 -0700

    Testing app.js

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ app.listen(3000, () => {
 });
 
 app.get('/', function(req, res) {
-    res.send("Yep it's working");
+    //res.send('Hi Love');
+    res.sendFile('index.html', { root: '.' });
   });
 
   app.get('/love', (req, res) => {
     res.send('Hi Love');
   });
-  
+
+  app.get('/signup.html', function(req, res) {
+    //res.send('Hi Love');
+    res.sendFile('html/signup.html', { root: '.' });
+  });
