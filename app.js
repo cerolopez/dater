a7b140cae2f31be9d927b0e1d85e5bcc9bae219c
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-10-22 20:07:27 -0700

    Update env

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 app.use(express.static('public'));
 const { MongoClient } = require("mongodb");
-const uri = "mongodb+srv://fulltimers4real:ejg9wDDBJgNgvMw@3rdwheel.ohwtksk.mongodb.net/?retryWrites=true&w=majority";
-const databaseName = "daterdb";
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -14,12 +14,12 @@ app.listen(3000, () => {
     console.log('Server listening on 3000');
  });
 
-MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
+MongoClient.connect(process.env.URI, { useNewUrlParser: true }, (error, client) => {
   if (error) {
     return console.log("Connection failed for some reason");
   }
   console.log("Connection established - All well");
-  db = client.db(databaseName);
+  db = client.db(process.env.DATABASE_NAME);
   
 });
 
@@ -44,35 +44,3 @@ app.post('/sign-up', urlencodedParser, (req, res) => {
     res.sendStatus(201);
   });
 });
-
-// begin example code
-
-/*
-
-import express from "express";
-import bodyParser from "body-parser";
-import session from "express-session";
-
-import router from "./routes/routes.js";
-
-export const PORT = process.env.PORT || 3000;
-
-const app = express();
-
-app.use(session({ secret: "John Loves Web", cookie: { maxAge: 60000 } }));
-app.use(bodyParser.json()); // to support JSON-encoded bodies
-app.use(
-  bodyParser.urlencoded({
-    // to support URL-encoded bodies
-    extended: true,
-  })
-);
-
-app.use(router);
-
-app.use(express.static("frontend"));
-
-app.listen(PORT, () => {
-  console.log(`Listening for connections on port ${PORT}`);
-});
-*/
