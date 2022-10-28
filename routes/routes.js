0c4e073ed12a9bbb0f88ec4e85c5a08a67fa5f7d
tcrawley2 <62876986+tcrawley2@users.noreply.github.com>
2022-10-27 20:57:38 -0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -1,25 +1,69 @@
 import express from 'express';
-import myDB from '../db/MyMongoDB.js';
+let router = express.Router();
 
-export const PORT = process.env.PORT || 3000;
+import myDB from "../db/MyMongoDB.js";
 
-const router = express.Router();
+//Is this needed??
+//export const PORT = process.env.PORT || 3000;
+
+import bodyParser from "body-parser";
+
 
 // begin example code
 
-router.post('/login', async (req, res) => {
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+router.post('/sign-up', urlencodedParser, (req, res) => {
+
+  console.log("I've made it to the router")
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const result = myDB.createUser(name, email, password);
+
+  if (!result) {
+    console.log("Didn't work... sorry!");
+  } else {
+    console.log("Everything went well! Create user was successful");
+    res.sendStatus(201);
+  }
+
+  // db.collection('users').insertOne({
+  //   name: name,
+  //   email: email,
+  //   password: password
+  //  }, (err, result) => {
+  //   if (err) {
+  //     return console.log(err);
+  //   }
+
+  //   console.log('insert was successful - user added from app.js');
+  //   console.log(res);
+  //   res.sendStatus(201);
+  // });
+});
+
+router.post('/authenticate(.html)?', async (req, res) => {
   const user = req.body;
 
   // TODO check that we got the correct info
 
   if (await myDB.authenticate(user)) {
-    req.session.user = user.user;
-
-    res.redirect("/?msg=authenticated");
+    req.session.user = {user: user};
+    res.json({isLoggedIn: true, err: null});
+    console.log(req.session);
   } else {
-    res.redirect("/?msg=error authenticating");
+    req.session.user = null;
+    res.json({ isLoggedIn: false, err: "Wrong email or password." });
   }
 });
+
+// router.get('/dashboard(.html)?', async (req, res) => {
+//   if (req.session.user) {
+
+//   }
+// });
 
 router.get('/users', (req, res) => {
   res.send("Users will be here!");
