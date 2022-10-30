f78277ca5748f2689fe2b8f113eaef91f985da87
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-10-30 09:45:58 -0700

    Rendered date on frontend

--- public/scripts/clientDates.js
+++ public/scripts/clientDates.js
@@ -7,23 +7,27 @@ function ClientDates() {
     function renderDates (userDates) {
       // do something
       datesDiv.innerHTML = '';
-      console.log("I'm in renderDates: ", userDates);
-      console.log(`First date: ${userDates.at(0).date}`);
+      //console.log("I'm in renderDates: ", userDates);
+      //console.log(`First date: ${userDates.at(0).date}`);
 
-    }
 
-      /*
-      for (let step = 0; step < 7; step++) {
-          userDates = dates[step];
-          if (userDates !== undefined) {
-              console.log(userDates);
-          }
-      }
-    }
-    
-      
-      datesDiv.innerHTML = "";
-      console.log("render dates", dates);
+      const dDiv = document.createElement("div");
+      dDiv.innerHTML = `
+
+          <div class="d-flex justify-content-center">
+          <div class="card dash-card text-center">
+          <div class="card-body">
+            <h3 id="inverse-color"><a id="inverse-hyperlink" href="">${userDates.at(0).email.at(1)}</a></h3>
+            <p id="inverse-color">${userDates.at(0).date}</p>
+          </div>
+          </div>
+          </div>
+
+        `;
+
+        datesDiv.appendChild(dDiv);
+
+        /*
       for (let d of dates) {
         const dDiv = document.createElement("div");
   
@@ -32,7 +36,7 @@ function ClientDates() {
           <div class="d-flex justify-content-center">
           <div class="card dash-card text-center">
           <div class="card-body">
-            <h3 id="inverse-color"><a id="inverse-hyperlink" href="">${d}</a></h3>
+            <h3 id="inverse-color"><a id="inverse-hyperlink" href="">${userDates.at(0).date}</a></h3>
             <p id="inverse-color">[date goes here]</p>
           </div>
           </div>
@@ -42,8 +46,8 @@ function ClientDates() {
   
         datesDiv.appendChild(dDiv);
       }
+      */
     }
-    */
 
     async function fetchDates () {
         const res = await fetch('./getDates');
