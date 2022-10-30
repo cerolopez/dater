b04e115c315b1fa099298f19971e1aa89ef643f3
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-10-30 00:21:06 -0700

    Update datesDB.js

--- db/datesDB.js
+++ db/datesDB.js
@@ -5,19 +5,77 @@ function datesDB() {
     const url = process.env.URI || "mongodb://localhost:27017";
     const DB_NAME = "daterdb";
     const DATE_COLLECTION = "dates";
+    const USER_COLLECTION = "users";
     const PAGE_SIZE = 6;
 
 
-    datesDB.getDates = async function (query = {}) {
+    datesDB.getDatesByUser = async function (query = {}) {
+        let client;
+        console.log("first query is: ", query);
+
+        try {
+            client = new MongoClient(url);
+            const userCollection = client.db(DB_NAME).collection(USER_COLLECTION);
+            
+            const cursor = await userCollection.find(
+                {
+                    email: query.email
+                }).toArray();
+                const reggie = cursor[0];
+
+                console.log("reggie's email: ", reggie);
+
+                return datesDB.getDatesArray(reggie);
+            //return datesDB.getDatesArray(reggie);
+            // figure out how to query based on object ID
+            //.skip(PAGE_SIZE * page)
+            //.limit(PAGE_SIZE)
+        } finally {
+            console.log("getDates: closing DB connection");
+            client.close;
+        }
+    };
+
+    datesDB.getDatesArray = async function (query = {}) {
+        let client;
+        console.log("second query is: ", query);
+
+        try {
+            client = new MongoClient(url);
+            const datesCollection = client.db(DB_NAME).collection(DATE_COLLECTION);
+            
+            const myCursor = await datesCollection.find(
+                {
+                    email: query.email
+                }
+            ).toArray();
+
+            return myCursor;
+            // figure out how to query based on object ID
+            //.skip(PAGE_SIZE * page)
+            //.limit(PAGE_SIZE)
+        } finally {
+            console.log("getDates: closing DB connection");
+            client.close;
+        }
+    }
+
+
+    datesDB.getDate = async function (query = {}) {
         let client;
 
         try {
             client = new MongoClient(url);
             const datesCollection = client.db(DB_NAME).collection(DATE_COLLECTION);
-            console.log("searching for dates...", query);
             
-            // query will be user ID
-            return await datesCollection.find(query).toArray();
+            const result = await datesCollection.find(
+                {
+                    email: query.email
+                }).toArray();
+            console.log("query is: ", query);
+            console.log("result is: ", result);
+
+            return result;
             // figure out how to query based on object ID
             //.skip(PAGE_SIZE * page)
             //.limit(PAGE_SIZE)
