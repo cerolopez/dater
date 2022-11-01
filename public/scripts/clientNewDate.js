9776772c220600873cc773ac93497558e7a75de0
cerolopez <98296063+cerolopez@users.noreply.github.com>
2022-11-01 08:17:59 -0700

    Update clientNewDate.js

--- public/scripts/clientNewDate.js
+++ public/scripts/clientNewDate.js
@@ -1,11 +1,8 @@
-<<<<<<< Updated upstream
 import clientUtil from "./clientUtilities.js";
 
 function ClientNewDate () {
     const clientNewDate = {};
     const divMsg = document.querySelector("div#msg");
-
-    // What's this? --> const dateDiv = document.querySelector("div#newDateContent");
 
     clientNewDate.setupCreateDate = async () => {
         console.log("I'm in setupCreateDate");
@@ -35,78 +32,6 @@ function ClientNewDate () {
     }
 
     clientNewDate.getCurrentUser = clientUtil.getCurrentUser;
-    
-    // form.addEventListener("submit", async (evt) => {
-    //     evt.preventDefault();
-    //     console.log("Fetching new date ID");
-    //     res = await fetch("/new-date", {
-    //         method: "GET",
-    //     });
-    //     const newDate = await res.json();
-    // });
-
-    //   async function grabNewDateID () => {
-    //     console.log("Creating a date...");
-    //     const form = document.querySelector("form#new-date-form");
-    //     let res;
-    //     form.addEventListener("submit", async (evt) => {
-    //         evt.preventDefault();
-    //         console.log("Attempting to create new date");
-    //         try {
-    //             res = await fetch("/new-date", {
-    //                 method: "POST",
-    //                 body: new URLSearchParams(new FormData(form))
-    //             });
-    //             const resUser = await res.json();
-    //             if (resUser.isCreated) {
-    //                 clientUtil.redirect("dashboard");
-    //             } else {
-    //                 clientUtil.showMessage(divMsg, resUser.err);
-    //                 setTimeout(() => clientUtil.redirect("signup"), 2000);
-    //             }
-    //         } catch (err) {
-    //             console.log(err);
-    //         }
-    //     });
-    //   }
-=======
-function ClientNewDate () {
-    const clientNewDate = {};
-
-    const form = document.querySelector("form#new-date-form");
-    
-    form.addEventListener("submit", async (evt) => {
-        evt.preventDefault();
-        console.log("Fetching new date ID");
-        res = await fetch("/new-date");
-        const newDate = await res.json();
-    });
-
-      async function grabNewDateID () => {
-        console.log("Creating a date...");
-        const form = document.querySelector("form#new-date-form");
-        let res;
-        form.addEventListener("submit", async (evt) => {
-            evt.preventDefault();
-            console.log("Attempting to create new date");
-            try {
-                res = await fetch("/new-date", {
-                    method: "POST",
-                    body: new URLSearchParams(new FormData(form))
-                });
-                const resUser = await res.json();
-                if (resUser.isCreated) {
-                    clientUtil.redirect("dashboard");
-                } else {
-                    clientUtil.showMessage(divMsg, resUser.err);
-                    setTimeout(() => clientUtil.redirect("signup"), 2000);
-                }
-            } catch (err) {
-                console.log(err);
-            }
-        });
-      }
->>>>>>> Stashed changes
 
     return clientNewDate;
 }
